b171e4fb968516a306a1846f886a75c709ec4da3
Danial4452 <danialkozahmet0@gmail.com>
2026-03-31 13:39:55 +0500

    Lab9

---
+++ Lab9/my_django_project/api/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField(default=0) 
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    
+
+    image = models.TextField(default='') 
+    link = models.TextField(default='')
+    rating = models.FloatField(default=0.0)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count,
+            'is_active': self.is_active,
+            'image': self.image,
+            'link': self.link,
+            'rating': self.rating,
+            'likes': self.likes
+        }
